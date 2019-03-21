29988fe3bdad411d88ba13df45f88e1df20ee933
tatitanja <46087790+tatitanja@users.noreply.github.com>
2019-03-21 21:49:47 +0100

    Update run_analysis.R

--- run_analysis.R
+++ run_analysis.R
@@ -1,4 +1,4 @@
-###You should create one R script called run_analysis.R that does the following.
+###Create one R script called run_analysis.R that does the following.
 
 # 1.Merges the training and the test sets to create one data set.
 # 2.Extracts only the measurements on the mean and standard deviation for each measurement.
